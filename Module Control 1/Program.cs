faf2b8fce63e2c187c3d0b46d3881995081dddb8
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-07 17:42:50 +0200

    Добавьте файлы проекта.

---
+++ Module Control 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Control_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song.Songs = new Song[]
+            {
+                new Song {Name = "Not Afraid", Author = "Eminem", Genre = "Rap", Length = 3.49f },
+                new Song {Name = "Rise", Author = "Skillet", Genre = "Rock" },
+                new Song {Name = "My immortal", Author = "Evanescence", Genre = "Rock" },
+                new Song {Name = "Pain", Author = "Hollywood undead", Genre = "RapCore", Length = 2.51f },
+                new Song {Name = "Paradize lost", Author =  "Hollywood undead", Genre = "RapCore", Length = 4.35f}
+            };
+
+            foreach (var song in Song.Songs)
+            {
+                if (song.Length.HasValue)
+                {
+                    Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}, Length = {song.Length}");
+                }
+                else
+                {
+                    Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}");
+                }
+            }
+            // Task 1
+            Console.Write("\nEnter index of song you wanna edit name: ");
+            int index = int.Parse(Console.ReadLine());
+            Console.Write("Enter new name of song: ");
+            string newName = Console.ReadLine();
+            Song.Songs[index].Name = newName;
+
+            Song.PrintAll();
+            // Task 2
+            Song.TheLongest();
+            // Task 3
+            Console.Write("\nEnter genre of songs: ");
+            string genre = Console.ReadLine();
+            Song.PrintAll(genre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module Control 1/Song.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Control_1
+{
+    struct Song
+    {
+        public string Name;
+        public string Author;
+        public string Genre;
+        public float? Length;
+        public static Song[] Songs;
+
+        public static void PrintAll()
+        {
+            foreach (var song in Songs)
+            {
+                if (song.Length.HasValue)
+                {
+                    Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}, Length = {song.Length}");
+                }
+                else
+                {
+                    Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}");
+                }
+            }
+        }
+
+        public static void PrintAll(string genre)
+        {
+            foreach (var song in Songs)
+            {
+                if (song.Genre == genre)
+                {
+                    if (song.Length.HasValue)
+                    {
+                        Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}, Length = {song.Length}");
+                    } else
+                    {
+                        Console.WriteLine($"Name = {song.Name}, Author = {song.Author}, Genre = {song.Genre}");
+                    }
+                }               
+            }
+        }
+        public static void TheLongest()
+        {
+            float max = (float)Songs[0].Length;
+            for (int i = 1; i <= Songs.Length; i++)
+            {
+                if (i == Songs.Length)
+                {
+                    Console.WriteLine($"The longest song:\n {Songs[i - 1].Name}, {Songs[i - 1].Author}, {Songs[i - 1].Genre}, {Songs[i - 1].Length}");
+                    break;
+                }
+                if (Songs[i].Length > max)          
+                {
+                    max = (float)Songs[i].Length;
+                }
+            }
+        }
+    }
+}
