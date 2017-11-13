77a0b11938e292f166dd2101965b5921162620e8
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-13 02:40:52 +0200

    add struct Card

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BlackJack
+{
+    enum Suit { Clubs, Diamonds, Hearts, Spades }
+    enum Number
+    {
+        Six = 6,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack = 2,
+        Lady,
+        King,
+        Ace = 11
+    }
+
+    struct Card
+    {
+        public Number Number;
+        public Suit Suit;
+
+        public Card(Number number, Suit suit)
+        {
+            Number = number;
+            Suit = suit;
+        }
+
+        public static Card[] GenerateDeck(Card[] newDeck)
+        {
+            for (int i = 0; i < newDeck.Length;)
+            {
+                foreach (var suit in Enum.GetValues(typeof(Suit)))
+                {
+                    foreach (var number in Enum.GetValues(typeof(Number)))
+                    {
+                        newDeck[i] = new Card((Number) number, (Suit) suit);
+                        i++;
+                    }
+                }
+            }
+            return newDeck;
+        }
+
+        public static void ShuffleDeck(Card[] deck)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < deck.Length; i++)
+            {
+                int randomIndex = rand.Next(deck.Length);
+                int randomIndex2 = rand.Next(deck.Length);
+                Card temp = deck[randomIndex];
+                deck[randomIndex] = deck[randomIndex2];
+                deck[randomIndex2] = temp;
+            }
+        }
+
+        public static void AddToHand(ref Card[] hand, ref Card[] deck)
+        {
+            Card[] temp = new Card[hand.Length + 1];
+            for (int i = 0; i < hand.Length; i++)
+            {
+                temp[i] = hand[i];
+            }
+            hand = temp;
+            int lastIndex = hand.Length - 1;
+            hand[lastIndex] = deck[0]; // add 1 card from deck to hand
+
+            // remove this card from deck
+            temp = new Card[deck.Length - 1];
+            for (int i = 1; i < deck.Length; i++)
+            {
+                temp[i-1] = deck[i];
+            }
+            deck = temp;
+        }
+    }
+}
--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -10,7 +10,90 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
+            // create and shuffle a deck
+            Card[] deck = new Card[36];
+            deck = Card.GenerateDeck(deck);
+            Card.ShuffleDeck(deck);
+            foreach (Card card in deck)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}");
+            }
 
+            // two players receive two cards each
+            Card[] playersHand = new Card[0];
+            Card[] computersHand = new Card[0];
+            ConsoleKeyInfo answer;
+            do
+            {
+                Console.WriteLine("Who will receive first cards?\nPress on NumPad: 1 - player or 0 - computer");
+                answer = Console.ReadKey();
+                switch (answer.Key)
+                {
+                    case ConsoleKey.NumPad1:
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (i == 0)
+                            {
+                                Card.AddToHand(ref playersHand, ref deck);
+                                Card.AddToHand(ref playersHand, ref deck);
+                            }
+                            else
+                            {
+                                Card.AddToHand(ref computersHand, ref deck);
+                                Card.AddToHand(ref computersHand, ref deck);
+                            }
+                        }
+                        break;
+                    case ConsoleKey.NumPad0:
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (i == 1)
+                            {
+                                Card.AddToHand(ref playersHand, ref deck);
+                                Card.AddToHand(ref playersHand, ref deck);
+                            }
+                            else
+                            {
+                                Card.AddToHand(ref computersHand, ref deck);
+                                Card.AddToHand(ref computersHand, ref deck);
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\rPlease enter a correct value!");
+                        break;
+                }
+            } while (answer.Key != ConsoleKey.NumPad1 && answer.Key != ConsoleKey.NumPad0);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+                        
+            Console.WriteLine(new string('-', 30));
+            foreach (Card card in playersHand)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}");
+            }
+
+            Console.WriteLine(new string('-', 30));
+            foreach (Card card in computersHand)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}");
+            }
+
+            Console.WriteLine(new string('-', 30));
+            foreach (Card card in deck)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}");
+            }
+            Console.ReadKey();
         }
     }
 }
